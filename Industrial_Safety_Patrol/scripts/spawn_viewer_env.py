ba7946f3fabb081a8ea537a89d444efc498eb3e8
crab-ally <274044817+crab-ally@users.noreply.github.com>
2026-07-10 17:28:47 +0900

    시험코드

--- Industrial_Safety_Patrol/scripts/spawn_viewer_env.py
+++ Industrial_Safety_Patrol/scripts/spawn_viewer_env.py
@@ -5,7 +5,7 @@ import time
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent      # scripts/
-xml_path = BASE_DIR.parent / "worlds" / "patrol_env.xml"
+xml_path = BASE_DIR.parent / "scenes" / "patrol_env.xml"
 
 # 1. 모델 로드
 model = mujoco.MjModel.from_xml_path(str(xml_path))
@@ -13,31 +13,42 @@ data = mujoco.MjData(model)
 
 # 2. 순찰 중 장애물 감지 로직 [3, 4]
 def check_for_obstacles(data):
-    lidar_dist = float(data.sensor('lidar').data[0])
-    if lidar_dist < 1.0: # 1미터 이내 장애물 감지 시
+    lidar_dist = data.sensor('lidar').data[0]
+    if lidar_dist >= 0 and lidar_dist < 1.0: # 1미터 이내 장애물 감지 시
         return True, lidar_dist
     return False, lidar_dist
 
 # 3. 시뮬레이션 실행 (100Hz 루프) [5]
 with mujoco.viewer.launch_passive(model, data) as viewer:
+    viewer.cam.lookat[:] = [-3, -3, 0.5]
+    viewer.cam.distance = 3
+    viewer.cam.azimuth = 45
+    viewer.cam.elevation = -30
+
     print("터틀봇 실내 순찰 시뮬레이션 시작...")
     
     while viewer.is_running():
         step_start = time.time()
 
+        state = "move"
+
         # 기본 주행: 전진 명령
-        data.ctrl[0] = 1.0 
-        data.ctrl[1] = 1.0
+        data.ctrl[0] = -2.0 
+        data.ctrl[1] = -2.0
 
         # 장애물 감지 시 우회 또는 정지 로직 (Scenario 1) [4]
         is_hazard, dist = check_for_obstacles(data)
         if is_hazard:
             # 장애물 발견 시 왼쪽으로 회전하여 회피 시도
-            data.ctrl[0] = -0.5
-            data.ctrl[1] = 0.5
+            state = "detected"
+            data.ctrl[0] = -1
+            data.ctrl[1] = 3
             if int(data.time * 10) % 5 == 0:
                 print(f"[경고] 장애물 감지! 거리: {dist:.2f}m - 우회 경로 탐색 중...")
 
+        if int(data.time * 10) % 5 == 0 and state == "move":
+            print("전진합니다")
+    
         mujoco.mj_step(model, data)
         viewer.sync()
 
